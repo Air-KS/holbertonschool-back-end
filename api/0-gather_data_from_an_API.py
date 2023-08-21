dce76072f6a76ae75cf749e5dec7d65cd041a7d7
Air-KS <kevinrogeret@gmail.com>
2023-08-21 12:53:06 +0000

    Refonte task 0, problem cheaker

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,32 +1,18 @@
 #!/usr/bin/python3
-""" script that, using this REST API, for a given employee ID """
+"""API request for employee name and todos completed"""
+
 import requests
 import sys
 
-
-def print_todo_progress(employee_ID):
-    BASE_URL = "https://jsonplaceholder.typicode.com/"
-
-    # Récupère le nom de l'employée
-    user_data = requests.get("{}users/{}".format
-                             (BASE_URL, employee_ID)).json()
-
-    # Récupère les TODOs pour cet employé
-    todos_data = requests.get("{}todos?userId={}".format
-                              (BASE_URL, employee_ID)).json()
-
-    # Filtre les tâches terminées
-    done_tasks = [task for task in todos_data if task["completed"]]
-
-    # Afficher les informations
-    print("Employee {} is done with tasks {}/{}:".format
-          (user_data["name"], len(done_tasks), len(todos_data)))
-    for task in done_tasks:
-        print("\t {}".format(task["title"]))
-
-
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print("Veuillez fournir un ID d'employé.")
-    else:
-        print_todo_progress(int(sys.argv[1]))
+    url = "https://jsonplaceholder.typicode.com"
+    user = requests.get(url + "/users/{}".format(sys.argv[1])).json()
+    todos = requests.get(url + "/todos", params={"userId": sys.argv[1]}).json()
+
+    total_tasks = len(todos)
+    completed_tasks = sum(1 for todo in todos if todo["completed"])
+
+    print("Employee {} is done with tasks({}/{}):".format(
+            user.get("name"), completed_tasks, total_tasks))
+
+    [print(f"\t {todo['title']}") for todo in todos if todo["completed"]]
