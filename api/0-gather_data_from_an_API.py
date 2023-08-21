2253801e31e16a9441099e97b44316c64fe4ec70
Air-KS <kevinrogeret@gmail.com>
2023-08-21 11:51:04 +0000

    Try task 0

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,35 +1,32 @@
 #!/usr/bin/python3
-"""
-Python script that, using a REST API, for a given employee ID.
-"""
-
-
+""" script that, using this REST API, for a given employee ID """
 import requests
 import sys
 
 
+def print_todo_progress(employee_ID):
+    BASE_URL = "https://jsonplaceholder.typicode.com/"
+
+    # Récupère le nom de l'employée
+    user_data = requests.get("{}users/{}".format
+                             (BASE_URL, employee_ID)).json()
+
+    # Récupère les TODOs pour cet employé
+    todos_data = requests.get("{}todos?userId={}".format
+                              (BASE_URL, employee_ID)).json()
+
+    # Filtre les tâches terminées
+    done_tasks = [task for task in todos_data if task["completed"]]
+
+    # Afficher les informations
+    print("Employee {} is done with tasks {}/{}:".format
+          (user_data["name"], len(done_tasks), len(todos_data)))
+    for task in done_tasks:
+        print("\t {}".format(task["title"]))
+
+
 if __name__ == "__main__":
-    employee_id = int(sys.argv[1])
-    base_url = "https://jsonplaceholder.typicode.com"
-    user_id = f"{base_url}/users/{employee_id}"
-    todo = f"{base_url}/todos?userId={employee_id}"
-
-    response_user = requests.get(user_id)
-    response_todo = requests.get(todo)
-
-    user_data = response_user.json()
-    todo_data = response_todo.json()
-
-    name = user_data['name']
-
-    todo_done = sum(1 for task in todo_data if task['completed'])
-    todo_count = len(todo_data)
-
-    list_task_complete = [task['title']
-                          for task in todo_data if task['completed']]
-
-    print("Employee {} is done with tasks({}/{}):"
-          .format(name, todo_done, todo_count))
-
-    for title in list_task_complete:
-        print("\t {}".format(title))
+    if len(sys.argv) < 2:
+        print("Veuillez fournir un ID d'employé.")
+    else:
+        print_todo_progress(int(sys.argv[1]))
